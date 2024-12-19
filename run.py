b384475c241ac1c71ae8b28f25d08607dc7b1ed9
andrey <palanelarty@gmail.com>
2024-12-19 20:00:16 +0300

    add base router

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 
+from app.handlers.base_handler import router as base_router
+
 
 async def main():
     load_dotenv()
@@ -11,6 +13,7 @@ async def main():
     bot = Bot(token=os.getenv('TOKEN'))
     dp = Dispatcher()
 
+    dp.include_routers(base_router)
     await dp.start_polling(bot)
 
 
